ce28d4b12e04f61848d5e5017e536c4cd05f80aa
nagypeterjob <nagypeterjob@gmail.com>
2018-05-15 23:03:24 +0200

    feat(service): add cors to service

--- service/app/main.py
+++ service/app/main.py
@@ -11,8 +11,10 @@ from pandas.io.json import json_normalize
 from keras.models import model_from_json
 import time
 from flask import jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/health', methods=['GET'])
 def health():
