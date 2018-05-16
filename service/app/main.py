416f0557fe002caa3d66740c6ff58f29a24246c2
nagypeterjob <nagypeterjob@gmail.com>
2018-05-16 22:41:34 +0200

    feat(service): add comments

--- service/app/main.py
+++ service/app/main.py
@@ -14,21 +14,29 @@ from flask import jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
+#Enable CORS so we can call the service via ajax request
+#TODO: Specify ips, so the service is not exposed
 CORS(app)
 
+#Health check: We (and the Orchestration tool) can easly test whether the service is up & running
 @app.route('/health', methods=['GET'])
 def health():
     return jsonify({'health': 'good'})
 
+#Endpoint that serves the pretrained model's prediction
 @app.route('/predict', methods=['POST', 'GET'])
 def predict():
     app.logger.info('[LOG]: Request hit')
     start = time.time()
+    #Load models from S3
+    #TODO: Implement service to prefech models after each successful training
     bin_model, mul_model = preload_models()
     end = time.time()
     app.logger.info('[LOG]: Models loaded in ' + str(end-start) + ' secs')
 
     data = request.get_json(silent=False)
+    
+    #switch between the binary/multi classification models
     is_binary = data['binary']
     app.logger.info(is_binary)
     if is_binary:
@@ -44,6 +52,8 @@ def predict():
 
     return jsonify({'response': pred.tolist()})
 
+
+#Pulling specific model & weight file from S3 Bucket
 def load_model(model_name, weight_name, w_name):
     s3_client = boto3.resource('s3')
     BUCKET = 'lmi-model-bucket'
@@ -62,7 +72,7 @@ def load_model(model_name, weight_name, w_name):
     weight_content = weights.get()['Body'].read()
 
     loaded_model = model_from_json(model_content)
-
+    #Write weight to disk as it cannot be passed "on the fly" as file 
     with open(w_name,'wb') as f:
         f.write(weight_content)
 
